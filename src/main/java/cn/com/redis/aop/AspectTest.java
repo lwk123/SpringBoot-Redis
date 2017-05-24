2c165b739c188a83100470045e974a687be7a72e
luocheng <1985636414@qq.com>
2017-05-24 20:07:15 +0800

    1

--- src/main/java/cn/com/redis/aop/AspectTest.java
+++ src/main/java/cn/com/redis/aop/AspectTest.java
@@ -4,6 +4,9 @@ import java.net.InetSocketAddress;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
@@ -23,110 +26,133 @@ import cn.com.redis.untils.RedisUtils2;
 
 public class AspectTest {
 	@Pointcut("execution(public * com.redis.controller.**.*(..))")
-    private void controllerAspect(){}
+	private void controllerAspect() {
+	}
 
-    @Before(value = "controllerAspect()")
-    public void methodBefore(JoinPoint joinPoint){
-    	// 创建链接
-    			CanalConnector connector = CanalConnectors
-    					.newSingleConnector(new InetSocketAddress(AddressUtils.getHostIp(), 11111), "example", "", "");
-    			int batchSize = 1000;
-    			try {
-    				connector.connect();
-    				connector.subscribe(".*\\..*");
-    				connector.rollback();
-    				while (true) {
-    					Message message = connector.getWithoutAck(batchSize); // 获取指定数量的数据
-    					long batchId = message.getId();
-    					int size = message.getEntries().size();
-    					if (batchId == -1 || size == 0) {
-    						try {
-    							Thread.sleep(1000);
-    						} catch (InterruptedException e) {
-    							e.printStackTrace();
-    						}
-    					} else {
-    						printEntry(message.getEntries());
-    					}
+	@Before(value = "controllerAspect()")
+	public void methodBefore(JoinPoint joinPoint) {
+		connectCanel();
+	}
 
-    					connector.ack(batchId); // 提交确认
-    					// connector.rollback(batchId); // 处理失败, 回滚数据
-    				}
+	@AfterReturning(returning = "o", pointcut = "controllerAspect()")
+	public void methodAfterReturning(Object o) {
+		connectCanel();
+	}
+	
+	@Around("controllerAspect()")
+    public void methodAround(ProceedingJoinPoint joinPoint){
+		connectCanel();
+    }
+	
+	public void connectCanel() {
+		// 创建链接
+		CanalConnector connector = CanalConnectors.newSingleConnector(
+				new InetSocketAddress(AddressUtils.getHostIp(), 11111),
+				"example", "", "");
+		int batchSize = 1000;
+		try {
+			connector.connect();
+			connector.subscribe(".*\\..*");
+			connector.rollback();
+			while (true) {
+				Message message = connector.getWithoutAck(batchSize); // 获取指定数量的数据
+				long batchId = message.getId();
+				int size = message.getEntries().size();
+				if (batchId == -1 || size == 0) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				} else {
+					printEntry(message.getEntries());
+				}
 
-    			} finally {
-    				connector.disconnect();
-    			}
-    		}
+				connector.ack(batchId); // 提交确认
+				// connector.rollback(batchId); // 处理失败, 回滚数据
+			}
 
-    		private static void printEntry(List<Entry> entrys) {
-    			for (Entry entry : entrys) {
-    				if (entry.getEntryType() == EntryType.TRANSACTIONBEGIN
-    						|| entry.getEntryType() == EntryType.TRANSACTIONEND) {
-    					continue;
-    				}
+		} finally {
+			connector.disconnect();
+		}
+	}
 
-    				RowChange rowChage = null;
-    				try {
-    					rowChage = RowChange.parseFrom(entry.getStoreValue());
-    				} catch (Exception e) {
-    					throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(),
-    							e);
-    				}
+	private static void printEntry(List<Entry> entrys) {
+		for (Entry entry : entrys) {
+			if (entry.getEntryType() == EntryType.TRANSACTIONBEGIN
+					|| entry.getEntryType() == EntryType.TRANSACTIONEND) {
+				continue;
+			}
 
-    				EventType eventType = rowChage.getEventType();
-    				System.out.println(String.format("================> binlog[%s:%s] , name[%s,%s] , eventType : %s",
-    						entry.getHeader().getLogfileName(), entry.getHeader().getLogfileOffset(),
-    						entry.getHeader().getSchemaName(), entry.getHeader().getTableName(), eventType));
+			RowChange rowChage = null;
+			try {
+				rowChage = RowChange.parseFrom(entry.getStoreValue());
+			} catch (Exception e) {
+				throw new RuntimeException(
+						"ERROR ## parser of eromanga-event has an error , data:"
+								+ entry.toString(),
+						e);
+			}
 
-    				for (RowData rowData : rowChage.getRowDatasList()) {
-    					if (eventType == EventType.DELETE) {
-    						redisDelete(rowData.getBeforeColumnsList());
-    					} else if (eventType == EventType.INSERT) {
-    						redisInsert(rowData.getAfterColumnsList());
-    					} else {
-    						System.out.println("-------> before");
-    						printColumn(rowData.getBeforeColumnsList());
-    						System.out.println("-------> after");
-    						redisUpdate(rowData.getAfterColumnsList());
-    					}
-    				}
-    			}
-    		}
+			EventType eventType = rowChage.getEventType();
+			System.out.println(String.format(
+					"================> binlog[%s:%s] , name[%s,%s] , eventType : %s",
+					entry.getHeader().getLogfileName(),
+					entry.getHeader().getLogfileOffset(),
+					entry.getHeader().getSchemaName(),
+					entry.getHeader().getTableName(), eventType));
 
-    		private static void printColumn(List<Column> columns) {
-    			for (Column column : columns) {
-    				System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
-    			}
-    		}
+			for (RowData rowData : rowChage.getRowDatasList()) {
+				if (eventType == EventType.DELETE) {
+					redisDelete(rowData.getBeforeColumnsList());
+				} else if (eventType == EventType.INSERT) {
+					redisInsert(rowData.getAfterColumnsList());
+				} else {
+					System.out.println("-------> before");
+					printColumn(rowData.getBeforeColumnsList());
+					System.out.println("-------> after");
+					redisUpdate(rowData.getAfterColumnsList());
+				}
+			}
+		}
+	}
 
-    		private static void redisInsert(List<Column> columns) {
-    			JSONObject json = new JSONObject();
-    			for (Column column : columns) {
-    				json.put(column.getName(), column.getValue());
-    			}
-    			if (columns.size() > 0) {
-    				RedisUtils2.stringSet("user:" + columns.get(0).getValue(), json.toJSONString());
-    			}
-    		}
+	private static void printColumn(List<Column> columns) {
+		for (Column column : columns) {
+			System.out.println(column.getName() + " : " + column.getValue()
+					+ "    update=" + column.getUpdated());
+		}
+	}
 
-    		private static void redisUpdate(List<Column> columns) {
-    			JSONObject json = new JSONObject();
-    			for (Column column : columns) {
-    				json.put(column.getName(), column.getValue());
-    			}
-    			if (columns.size() > 0) {
-    				RedisUtils2.stringSet("user:" + columns.get(0).getValue(), json.toJSONString());
-    			}
-    		}
+	private static void redisInsert(List<Column> columns) {
+		JSONObject json = new JSONObject();
+		for (Column column : columns) {
+			json.put(column.getName(), column.getValue());
+		}
+		if (columns.size() > 0) {
+			RedisUtils2.stringSet("user:" + columns.get(0).getValue(),
+					json.toJSONString());
+		}
+	}
 
-    		private static void redisDelete(List<Column> columns) {
-    			JSONObject json = new JSONObject();
-    			for (Column column : columns) {
-    				json.put(column.getName(), column.getValue());
-    			}
-    			if (columns.size() > 0) {
-    				RedisUtils2.delKey("user:" + columns.get(0).getValue());
-    			}
-    		}
+	private static void redisUpdate(List<Column> columns) {
+		JSONObject json = new JSONObject();
+		for (Column column : columns) {
+			json.put(column.getName(), column.getValue());
+		}
+		if (columns.size() > 0) {
+			RedisUtils2.stringSet("user:" + columns.get(0).getValue(),
+					json.toJSONString());
+		}
+	}
 
+	private static void redisDelete(List<Column> columns) {
+		JSONObject json = new JSONObject();
+		for (Column column : columns) {
+			json.put(column.getName(), column.getValue());
+		}
+		if (columns.size() > 0) {
+			RedisUtils2.delKey("user:" + columns.get(0).getValue());
+		}
+	}
 }
